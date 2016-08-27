837dea6649664d90359b44165d7e2a184b8980cb
Mauricio Mejía <jemmejiaca@unal.edu.co>
2016-08-26 19:28:27 -0500

    The basic structure of the application has been established

---
+++ src/java/presentation/bean/buyOrder.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentation.bean;
+
+/**
+ * TODO: Please delete me as soon as you can!
+ * @author mauricio
+ */
+public class buyOrder {
+    
+}
