aad2ea59e8d96981b0c778ed34ecb273edf15cef
Mauricio Mejía <jemmejiaca@unal.edu.co>
2016-09-02 07:42:30 -0500

    Rename some classes and methods about user creation again

---
+++ src/java/businessLogic/controller/HandleEmployee.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businessLogic.controller;
+
+/**
+ *
+ * @author mauricio
+ */
+public class HandleEmployee {
+    
+    /* TO DO
+    public String createEmployee(String id, Strin name, String role) {
+        
+    }*/
+    
+}
